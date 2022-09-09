3c1d9180253b32e085f0372881e51bc9c651c861
Kurtulus M. Akkaya <akkaya040@gmail.com>
2022-09-09 14:45:15 +0300

    Hook Files Were Added

---
+++ SpecFlowAutomation.Specs/Hooks/Hook.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using SpecFlowAutomation.Specs.Drivers;
+
+namespace SpecFlowAutomation.Specs.Hooks
+{
+    //public class Hook : IDisposable
+    [Binding]
+    public class Hook
+    {
+        private readonly BrowserDriver _browserDriver;
+
+        //private readonly IObjectContainer _objectContainer;
+        private IWebDriver _webdriver = null!;
+
+
+        //public Hook(IObjectContainer objectContainer, BrowserDriver browserDriver)
+        public Hook(BrowserDriver browserDriver)
+        {
+            //_objectContainer = objectContainer;
+            _browserDriver = browserDriver;
+        }
+
+
+        //Orjinal
+        [BeforeScenario("@Automation")]
+        //public static void BeforeScenario(BrowserDriver browserDriver)
+        public void BeforeScenario()
+        {
+            _webdriver = _browserDriver.GetIWebDriver();
+            //_objectContainer.RegisterInstanceAs<IWebDriver>(_webdriver);
+        }
+
+        [AfterScenario("@Automation")]
+        public void AfterScenario()
+        {
+            _webdriver.Quit();
+            _webdriver.Dispose();
+            //_objectContainer.Dispose();
+            _browserDriver.Dispose();
+        }
+
+
+        [AfterStep]
+        public void AfterEachStep()
+        {
+        }
+    }
+}
---
+++ SpecFlowAutomation.Specs/Hooks/ScenarioHook.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using BoDi;
+using OpenQA.Selenium;
+using SpecFlowAutomation.Specs.Drivers;
+
+namespace SpecFlowAutomation.Specs.Hooks
+{
+    [Binding]
+    public sealed class ScenarioHook : BrowserDriver
+    {
+        private readonly IObjectContainer _container;
+        private readonly ScenarioContext _scenario;
+        private readonly FeatureContext _feature;
+        private readonly BrowserDriver _browserDriver;
+
+
+        private bool IsFailingScenario => _scenario.TestError != null;
+
+        public ScenarioHook(IObjectContainer container, ScenarioContext scenario, FeatureContext feature,
+            BrowserDriver browserDriver)
+        {
+            _container = container;
+            _scenario = scenario;
+            _feature = feature;
+            _browserDriver = browserDriver;
+        }
+
+        [AfterStep("@Automation")]
+        public void AfterStepTasks()
+        {
+            var featureName = _feature.FeatureInfo.Title;
+            var scenarioName = _scenario.ScenarioInfo.Title;
+            var stepName = _scenario.StepContext.StepInfo.Text;
+
+
+            Console.Out.WriteLine("| {0} | {1} | {2} |", featureName, scenarioName, stepName);
+            System.Diagnostics.Debug.WriteLine("| {0} | {1} | {2} |", featureName, scenarioName, stepName);
+
+            if (IsFailingScenario)
+            {
+                TakeScreenshot(_browserDriver.GetIWebDriver(), _scenario, _feature);
+            }
+
+            WaitForPageLoad();
+        }
+
+        private void WaitForPageLoad()
+        {
+            var wait = CreateWait(_browserDriver.GetIWebDriver());
+            wait.Until(driver =>
+                ((IJavaScriptExecutor) driver).ExecuteScript("return document.readyState").Equals("complete"));
+            //wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("nav img[alt='Felixo']")));
+        }
+    }
+}
